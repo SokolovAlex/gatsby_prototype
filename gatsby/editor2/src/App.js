e32460bcc21a4f28f4581602ada5e9202e52d0e0
sokolov alex <alex.sklv85@gmail.com>
2018-10-11 22:44:48 +0300

    new editor app

---
+++ gatsby/editor2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Layout from './components/layouts/common/common'
+
+class App extends Component {
+  render() {
+    return (
+        <Layout/>
+    );
+  }
+}
+
+export default App;
